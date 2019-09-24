6f6f4d60622841056e61ad42907cbe8cdd4b546c
dias-olr <rdiasoli97@gmail.com>
2019-09-24 23:29:23 +0200

    SwitchExample test

---
+++ lab1/src/test/java/SwitchExampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwitchExampleTest {
+
+    @Test
+    void switchExample() {
+        assertEquals(1,SwitchExample.switchExample("dog"));
+    }
+
+}
