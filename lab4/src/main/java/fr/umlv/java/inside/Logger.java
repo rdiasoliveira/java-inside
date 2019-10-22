74266e01a53a6069f365322ebbda270cc67c9783
dias-olr <rdiasoli97@gmail.com>
2019-10-22 10:27:52 +0200

    Revert "Adding Lab5 - First test"
    This reverts commit fdc5ffe54515ca21e71201b04aa8076d261d01ac.

--- lab4/src/main/java/fr/umlv/java/inside/Logger.java
+++ lab4/src/main/java/fr/umlv/java/inside/Logger.java
@@ -1,8 +1,6 @@
 package fr.umlv.java.inside;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MutableCallSite;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Objects;
 import java.util.function.Consumer;
@@ -57,17 +55,6 @@ public interface Logger {
             }
         };
     }
-
-    static final ClassValue<MutableCallSite> ENABLE_CALLSITES = new ClassValue<>() {
-        protected MutableCallSite computeValue(Class<?> type) {
-            return new MutableCallSite(MethodHandles.constant(boolean.class, true));
-        }
-    };
-
-    public static void enable(Class<?> declaringClass, boolean enable) {
-        ENABLE_CALLSITES.get(declaringClass).setTarget(MethodHandles.constant(boolean.class, enable));
-    }
-
 
     private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
         MethodHandle m;
