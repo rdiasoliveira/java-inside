59d57e8ada681a8f5d04472dc95cd486ea25bfaf
dias-olr <rdiasoli97@gmail.com>
2019-10-27 12:04:04 +0100

    stringSwitch3 & createMHFromString3 w/ InliningCache

--- lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
+++ lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
@@ -3,6 +3,8 @@ package fr.umlv.java.inside;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.invoke.MutableCallSite;
+import java.util.List;
 
 public class StringSwitchExample {
 
@@ -45,6 +47,47 @@ public class StringSwitchExample {
                     mh);
         }
         return mh;
+    }
+
+    public static int stringSwitch3(String str) throws Throwable {
+        var mh = createMHFromStrings3("foo", "bar", "bazz");
+        return (int) mh.invokeExact(str);
+    }
+
+    public static MethodHandle createMHFromStrings3(String... matches) {
+        return new InliningCache(matches).dynamicInvoker();
+    }
+
+    static class InliningCache extends MutableCallSite {
+
+        private static final MethodHandle SLOW_PATH;
+        private final List<String> matches;
+
+        static {
+            try {
+                SLOW_PATH = MethodHandles.
+                        lookup().
+                        findVirtual(InliningCache.class, "slowPath", MethodType.methodType(int.class, String.class));
+            } catch (NoSuchMethodException | IllegalAccessException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        public InliningCache(String... matches) {
+            super(MethodType.methodType(int.class, String.class));
+            this.matches = List.of(matches);
+            setTarget(MethodHandles.insertArguments(SLOW_PATH, 0, this));
+        }
+
+        private int slowPath(String value) {
+            var index = matches.indexOf(value);
+            var mh = MethodHandles.guardWithTest(MethodHandles.insertArguments(STRINGS_EQUALS, 1, value),
+                    MethodHandles.dropArguments(MethodHandles.constant(int.class, index), 0, String.class),
+                    getTarget());
+            setTarget(mh);
+            return index;
+        }
+
     }
 
 }
