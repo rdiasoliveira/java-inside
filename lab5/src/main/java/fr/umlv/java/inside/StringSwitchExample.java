fd453b4f9aafe7e8301c7c1e37ab12404d91ead1
dias-olr <rdiasoli97@gmail.com>
2019-10-27 12:02:57 +0100

    stringSwitch2 & createMHFromString2

--- lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
+++ lab5/src/main/java/fr/umlv/java/inside/StringSwitchExample.java
@@ -1,13 +1,50 @@
 package fr.umlv.java.inside;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 public class StringSwitchExample {
 
     public static int stringSwitch(String str) {
         switch (str) {
-            case ("foo"): return 0;
-            case ("bar"): return 1;
-            case ("bazz"): return 2;
-            default: return -1;
+            case ("foo"):
+                return 0;
+            case ("bar"):
+                return 1;
+            case ("bazz"):
+                return 2;
+            default:
+                return -1;
         }
     }
+
+    private static final MethodHandle STRINGS_EQUALS;
+
+    static {
+        var lookup = MethodHandles.lookup();
+        try {
+            STRINGS_EQUALS =
+                    lookup.findVirtual(String.class, "equals", MethodType.methodType(boolean.class, Object.class));
+        } catch (NoSuchMethodException | IllegalAccessException e) {
+            throw new AssertionError(e);
+        }
+    }
+
+    public static int stringSwitch2(String str) throws Throwable {
+        var mh = createMHFromStrings2("foo", "bar", "bazz");
+        return (int) mh.invokeExact(str);
+    }
+
+    public static MethodHandle createMHFromStrings2(String ... strings) {
+        var mh = MethodHandles.dropArguments(MethodHandles.constant(int.class, -1), 0, String.class);
+        for(var i = 0; i < strings.length; i++) {
+            mh = MethodHandles.guardWithTest(
+                    MethodHandles.insertArguments(STRINGS_EQUALS, 1, strings[i]),
+                    MethodHandles.dropArguments(MethodHandles.constant(int.class, i), 0, String.class),
+                    mh);
+        }
+        return mh;
+    }
+
 }
