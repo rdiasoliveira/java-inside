445eb154c2f6d89ebc2efef2ffb3853bbe0e764e
dias-olr <rdiasoli97@gmail.com>
2019-10-29 10:04:50 +0100

    Trying continuation.

--- lab6/src/main/java/fr/umlv/java/inside/Example1.java
+++ lab6/src/main/java/fr/umlv/java/inside/Example1.java
@@ -4,6 +4,15 @@ public class Example1 {
 
     public static void main(String[] args) {
 
+        var scope = new ContinuationScope("hello1");
+        var continuation = new Continuation(scope, () -> {
+            Continuation.yield(scope);
+            System.out.println("hello continuation");
+        });
+
+        continuation.run();
+        continuation.run();
+
     }
 
 }
