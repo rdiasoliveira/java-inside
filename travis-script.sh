121e33efa37a4338cbe893051896aa046b52d665
dias-olr <rdiasoli97@gmail.com>
2019-11-04 00:16:42 +0100

    Changing script.

--- travis-script.sh
+++ travis-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-if ["$TARGET" = "lab6"]; then
-    if ["$OSTYPE" = "darwin17"]; then
+if [ "$TARGET" = "lab6" ]; then
+    if [ "$OSTYPE" = "darwin17" ]; then
         wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-osx.tar.gz
         tar xvf jdk-14-loom-osx.tar.gz
     else
@@ -12,4 +12,4 @@ if ["$TARGET" = "lab6"]; then
 fi
 
 cd $TARGET
-mvn clean package
+mvn package
